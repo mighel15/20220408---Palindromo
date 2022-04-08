abae419dbd2bae78cecee585ae0dad4caa750356
Miguel Aceituno Rojo <Miguel Aceituno Rojo@DESKTOP-D73J91I>
2022-04-08 10:28:47 -0500

    Se agrega una función para eliminar los espacios en blanco

--- 20220408 - Palindromo/20220408 - Palindromo.cpp
+++ 20220408 - Palindromo/20220408 - Palindromo.cpp
@@ -3,25 +3,40 @@
 
 using namespace std;
 
+
+
 int main()
 {
-    int indiceFinal;
-    char frase[50];
-    bool esPalindromo = true;
-    cout << "Ingrese la cadena que desea evaluar: " << endl;
-    cin.getline(frase,50,'\n');    
-    indiceFinal = strlen(frase) - 1;
-    for (int i = 0; i < strlen(frase)/2; i++)
-    {
-        if (frase[i] != frase[indiceFinal])
-        {
-            esPalindromo = false;
-            break;
-        }
-        indiceFinal--;
-    }
-    if (esPalindromo)
-        cout << "Es palindromo" << endl;
-    else
-        cout << "No es palindromo" << endl;
+	int indiceFinal;
+	char frase[50];
+	char frasePreProcesada[50];
+	bool esPalindromo = true;
+	int indiceNuevo = 0;
+
+	cout << "Ingrese la cadena que desea evaluar: " << endl;
+	cin.getline(frase, 50, '\n');
+
+	for (int i = 0; i < strlen(frase); i++)
+	{
+		if (frase[i] != ' ')
+			frasePreProcesada[indiceNuevo++] = frase[i];
+	}
+	frasePreProcesada[indiceNuevo] = '\0';
+
+	
+	indiceFinal = strlen(frasePreProcesada) - 1;
+
+	for (int i = 0; i < strlen(frasePreProcesada) / 2; i++)
+	{
+		if (frasePreProcesada[i] != frasePreProcesada[indiceFinal])
+		{
+			esPalindromo = false;
+			break;
+		}
+		indiceFinal--;
+	}
+	if (esPalindromo)
+		cout << "Es palindromo" << endl;
+	else
+		cout << "No es palindromo" << endl;
 }
